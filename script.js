171eb571ea0c0cec36a696aa196c96724df7aef3
kame2174 <kamesei5@gmail.com>
2026-06-29 23:30:38 +0900

    Add files via upload
    feat: KPI settings screen

---
+++ script.js
@@ -0,0 +1,13 @@
+const APP = {
+
+    VERSION: "2.1.0",
+
+    DEFAULT_KPI: [
+        { name:"カケホ", rate:35 },
+        { name:"最強保護", rate:30 },
+        { name:"U-NEXT", rate:13 }
+    ]
+
+};
+
+window.addEventListener("DOMContentLoaded", init);
